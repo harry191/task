8d62b25c8f571b34f201283cfc3fad3ec910b59c
H <thisisafakeemail@lol.com>
2020-01-09 13:36:54 +0000

    commit

---
+++ src/main/java/com/qa/task/MergeJoin.java
@@ -0,0 +1,20 @@
+package com.qa.task;
+
+public class MergeJoin {
+
+	// Both left and right must be sorted 
+	public static DataSet merge(DataSet left, DataSet right) {
+		DataSet output = new DataSet();
+		while (!left.isPastEnd() || !right.isPastEnd()) {
+			if (left.getKey() == right.getKey()) {
+				output.add(left.getKey());
+				left.advance();
+				right.advance();
+			} else if (left.getKey() < right.getKey())
+				left.advance();
+			else
+				right.advance();
+		}
+		return output;
+	}
+}
